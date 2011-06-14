de3c0d830aa7583ff7c7a78fb9b8d42304dd79fc
hieuvui214 <hieuvui214@yahoo.com>
2011-06-14 17:55:21 +0000

    Testing layout with background, using ActivityGroup, state list (button), option menu, context menu, submenu. (The last partial commit)

---
+++ LayoutDemo/src/com/layoutdemo/activity/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.layoutdemo.activity;
+
+import com.layoutdemo.R;
+import com.layoutdemo.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+	}
+}
