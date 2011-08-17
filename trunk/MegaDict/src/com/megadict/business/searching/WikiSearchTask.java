33bc79aa80daf2fe2577697ac54b310d05872987
hieuvui214@yahoo.com <hieuvui214@yahoo.com@dd0a8cbd-19d6-256d-402b-60c49949765a>
2011-08-17 17:39:35 +0000

    Fix history.

--- trunk/MegaDict/src/com/megadict/business/searching/WikiSearchTask.java
+++ trunk/MegaDict/src/com/megadict/business/searching/WikiSearchTask.java
@@ -1,19 +1,24 @@
 package com.megadict.business.searching;
 
 
+
 public class WikiSearchTask extends BaseSearchTask {
+
+
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+	}
+
 	@Override
 	protected String doInBackground(final String... params) {
 		return null;
 	}
 
 	@Override
-	protected void onPreExecute() {
-
-	}
-
-	@Override
 	protected void onPostExecute(final String result) {
+		super.onPostExecute(result);
+
 
 	}
 }
