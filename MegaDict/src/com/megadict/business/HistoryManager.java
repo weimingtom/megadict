8ec2c61a8e24bcba278b8243ead24cf2721468e6
hieuvui214 <hieuvui214@yahoo.com>
2011-08-17 17:39:35 +0000

    Fix history.

--- MegaDict/src/com/megadict/business/HistoryManager.java
+++ MegaDict/src/com/megadict/business/HistoryManager.java
@@ -1,24 +1,30 @@
 package com.megadict.business;
 
 import java.util.Collection;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class HistoryManager {
-	private final SortedSet<String> historySet = new TreeSet<String>();
+	private final Set<String> historySet = new LinkedHashSet<String>();
 
 	public HistoryManager() {
-		historySet.add("person");
-		historySet.add("personal");
-		historySet.add("something");
-		historySet.add("damn");
+		//		historySet.add("person");
+		//		historySet.add("pers");
+		//		historySet.add("person");
+		//		historySet.add("personal");
+		//		historySet.add("pers");
+		//		historySet.add("something");
+		//		historySet.add("test");
+		//		historySet.add("something");
 	}
 
 	public void save(final String word) {
-		historySet.add(word);
+		if(!historySet.contains(word)) {
+			historySet.add(word);
+		}
 	}
 
-	public SortedSet<String> getWordSet() {
+	public Set<String> getWordSet() {
 		return historySet;
 	}
 
