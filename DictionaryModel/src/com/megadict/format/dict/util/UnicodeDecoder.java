4ad7beb90558aa6f9dcf1ad2a540b9940fcbc2c6
genzers <genzers@gmail.com>
2011-09-07 01:23:25 +0000

    - Fixed Bug #13: wrong result when looking up "thông minh", "học tập",.... Cause: java.nio.charset.CharsetDecoder's default behavior is stop processing if it detects the input contains malformed or unmapped characters. Changing the behavior by setting onMalformedInput & onUnmappableCharacter to CodingErrorAction.REPLACE will solve the problem.

--- DictionaryModel/src/com/megadict/format/dict/util/UnicodeDecoder.java
+++ DictionaryModel/src/com/megadict/format/dict/util/UnicodeDecoder.java
@@ -5,21 +5,28 @@ import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
 
 import com.megadict.exception.OperationFailedException;
 
 public class UnicodeDecoder {
 
     private static final Charset UNICODE_CHARSET = Charset.forName("UTF-8");
-    private static final CharsetDecoder decoder = UNICODE_CHARSET.newDecoder();
-    
+    private static final CharsetDecoder DECODER;
+
+    static {
+        DECODER = UNICODE_CHARSET.newDecoder()
+                .onMalformedInput(CodingErrorAction.REPLACE)
+                .onUnmappableCharacter(CodingErrorAction.REPLACE);
+    }
+
     private UnicodeDecoder() {
     }
 
     public synchronized static CharBuffer decode(ByteBuffer input) {
         try {
-            CharBuffer decoded = decoder.decode(input);
-            decoder.reset();
+            CharBuffer decoded = DECODER.decode(input);
+            DECODER.reset();
             return decoded;
         } catch (CharacterCodingException cce) {
             throw new OperationFailedException("decoding a ByteBuffer input", cce);
@@ -27,8 +34,8 @@ public class UnicodeDecoder {
     }
 
     public synchronized static void decode(ByteBuffer in, CharBuffer out, boolean endOfInput) {
-        decoder.decode(in, out, true);
-        decoder.flush(out);
-        decoder.reset();
+        DECODER.decode(in, out, true);
+        DECODER.flush(out);
+        DECODER.reset();
     }
 }
