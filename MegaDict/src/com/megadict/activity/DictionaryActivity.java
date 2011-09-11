d2f1f166e151ec4db708f481814bc79d152ebe82
hieuvui214 <hieuvui214@yahoo.com>
2011-09-11 17:12:13 +0000

    - Fix crashing when rotate the screen. - Each RecommendTask recommends 100 words. - Max recommended words is 300 words.

--- MegaDict/src/com/megadict/activity/DictionaryActivity.java
+++ MegaDict/src/com/megadict/activity/DictionaryActivity.java
@@ -7,8 +7,8 @@ import java.util.Set;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -39,10 +39,13 @@ import com.megadict.utility.Utility;
 import com.megadict.widget.ResultView;
 
 public final class DictionaryActivity extends AbstractActivity {
-	private static final String TAG = "DictionaryActivity";
-
 	// Activity control variables.
+	private Button searchButton, pronounceButton;
 	private ResultView resultView;
+	private AutoCompleteTextView searchBar;
+	private ProgressBar progressBar;
+	private ResultTextMaker resultTextMaker;
+
 
 	// Member variables
 	private DictionaryComponent dictionaryComponent;
@@ -58,12 +61,21 @@ public final class DictionaryActivity extends AbstractActivity {
 	}
 
 	@Override
+	public Object onRetainNonConfigurationInstance() {
+		// Retain business component.
+		return businessComponent;
+	}
+
+	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.d(TAG, "onCreate");
 
-		// Init everthing!
-		initSomething();
+		initUIs();
+		initBottomButtons();
+		initResultTextMakerAndResultView();
+		initDictionaryComponent();
+		initBusinessComponent();
+		initInitializers();
 
 		// Load language when first start app.
 		Utility.updateLocale(this, LanguagePreference.newInstance(this).getLanguage());
@@ -81,9 +93,13 @@ public final class DictionaryActivity extends AbstractActivity {
 	}
 
 	@Override
+	public void onConfigurationChanged(final Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
 	protected void onStart() {
 		super.onStart();
-		Log.d(TAG, "onStart");
 
 		// Update dictionary models.
 		scanner.updateDictionaryModels(dictionaryComponent);
@@ -92,7 +108,6 @@ public final class DictionaryActivity extends AbstractActivity {
 	@Override
 	protected void onStop() {
 		super.onStop();
-		Log.d(TAG, "onStop");
 
 		// Reset languageChanged.
 		languageChanged = false;
@@ -103,23 +118,20 @@ public final class DictionaryActivity extends AbstractActivity {
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		Log.d(TAG, "onDestroy");
 
 		pronounceButtonInitializer.shutDownTTSSpeaker();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(final Menu menu) {
-		Log.d(TAG, "onCreateOptionsMenu");
-
+		super.onCreateOptionsMenu(menu);
 		inflateMenu(menu);
 		return true;
 	}
 
 	@Override
 	public boolean onPrepareOptionsMenu(final Menu menu) {
-		Log.d(TAG, "onPrepareOptionsMenu");
-
+		super.onPrepareOptionsMenu(menu);
 		// Redraw main menu.
 		if(languageChanged) {
 			menu.clear();
@@ -163,88 +175,6 @@ public final class DictionaryActivity extends AbstractActivity {
 	}
 
 	// ========================= Private functions ======================= //
-	private void initSomething() {
-		// Create UI components.
-		final ProgressBar progressBar =
-				(ProgressBar) findViewById(R.id.progressBar);
-		final Button searchButton = (Button) findViewById(R.id.searchButton);
-		final Button pronounceButton = (Button)findViewById(R.id.pronounceButton);
-		final AutoCompleteTextView searchBar = (AutoCompleteTextView) findViewById(R.id.searchEditText);
-		// Bottom buttons.
-		final Button manageButton = (Button)findViewById(R.id.manageButton);
-		final Button historyButton = (Button)findViewById(R.id.historyButton);
-		final Button settingButton = (Button)findViewById(R.id.selectButton);
-		final Button moreButton = (Button)findViewById(R.id.moreButton);
-		bottomButtonMap.put(manageButton, R.string.manageButtonLabel);
-		bottomButtonMap.put(historyButton, R.string.historyButtonLabel);
-		bottomButtonMap.put(settingButton, R.string.selectButtonLabel);
-		bottomButtonMap.put(moreButton, R.string.moreButtonLabel);
-
-		// Result text maker.
-		final ResultTextMaker resultTextMaker =	new ResultTextMaker(getAssets());
-		resultView = new ResultView(this);
-
-		// Create dictionaryComponent which contain UI components.
-		dictionaryComponent =
-				new DictionaryComponent.Builder().
-				searchButton(searchButton).
-				pronounceButton(pronounceButton).
-				searchBar(searchBar).
-				resultView(resultView).
-				resultTextMaker(resultTextMaker).
-				progressBar(progressBar).
-				context(this).
-				bottomButtons(new ArrayList<Button>(bottomButtonMap.keySet())).build();
-
-		// Get scanner from application.
-		scanner = ((MegaDictApp) getApplication()).scanner;
-
-		// Create searcher, recommender and businessComponent.
-		final WordRecommender recommender =
-				new WordRecommender(scanner.getDictionaryModels(), dictionaryComponent);
-		final WordSearcher searcher =
-				new WordSearcher(scanner.getDictionaryModels(), dictionaryComponent);
-		businessComponent =
-				new BusinessComponent(searcher, recommender);
-		// Register observers for scanner.
-		scanner.addObserver(searcher);
-		scanner.addObserver(recommender);
-
-		// Init search button.
-		final SearchButtonInitializer searchButtonInitializer =
-				new SearchButtonInitializer(businessComponent, dictionaryComponent);
-		searchButtonInitializer.init();
-
-		// Init pronounce button.
-		pronounceButtonInitializer = new PronounceButtonInitializer(dictionaryComponent);
-
-		// Init search bar.
-		final SearchBarInitializer searchBarInitializer =
-				new SearchBarInitializer(businessComponent, dictionaryComponent);
-		searchBarInitializer.init();
-		searchBarInitializer.addObserver(recommender);
-
-		// Init Result view.
-		final ResultViewInitializer resultViewInitializer =
-				new ResultViewInitializer(businessComponent, dictionaryComponent);
-		resultViewInitializer.init();
-		resultViewInitializer.addObserver(recommender);
-
-		// Init history retriever.
-		historyDisplayer = new HistoryDisplayer(businessComponent, dictionaryComponent);
-		historyDisplayer.init();
-		historyDisplayer.addObserver(recommender);
-
-		// Init result panel.
-		final LinearLayout resultPanel =
-				(LinearLayout) findViewById(R.id.resultPanel);
-		resultPanel.addView(resultView);
-
-		// Init bottom buttons.
-		final BottomButtonsInitializer bottomInitializer = new BottomButtonsInitializer(historyDisplayer, businessComponent, dictionaryComponent);
-		bottomInitializer.init();
-	}
-
 	private void doScanningStorage() {
 		scanner.scanStorage(dictionaryComponent);
 	}
@@ -265,4 +195,106 @@ public final class DictionaryActivity extends AbstractActivity {
 			button.setText(bottomButtonMap.get(button));
 		}
 	}
+
+
+
+
+	// ==================== Init functions =================//
+	private void initBottomButtons() {
+		final Button manageButton = (Button)findViewById(R.id.manageButton);
+		final Button historyButton = (Button)findViewById(R.id.historyButton);
+		final Button settingButton = (Button)findViewById(R.id.selectButton);
+		final Button moreButton = (Button)findViewById(R.id.moreButton);
+		bottomButtonMap.put(manageButton, R.string.manageButtonLabel);
+		bottomButtonMap.put(historyButton, R.string.historyButtonLabel);
+		bottomButtonMap.put(settingButton, R.string.selectButtonLabel);
+		bottomButtonMap.put(moreButton, R.string.moreButtonLabel);
+	}
+
+	private void initUIs() {
+		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+		searchButton = (Button) findViewById(R.id.searchButton);
+		pronounceButton = (Button)findViewById(R.id.pronounceButton);
+		searchBar = (AutoCompleteTextView) findViewById(R.id.searchEditText);
+	}
+
+	private void initResultTextMakerAndResultView() {
+		resultTextMaker = new ResultTextMaker(getAssets());
+		resultView = new ResultView(this);
+	}
+
+	private void initDictionaryComponent() {
+		dictionaryComponent =
+				new DictionaryComponent.Builder().
+				searchButton(searchButton).
+				pronounceButton(pronounceButton).
+				searchBar(searchBar).
+				resultView(resultView).
+				resultTextMaker(resultTextMaker).
+				progressBar(progressBar).
+				context(this).
+				bottomButtons(new ArrayList<Button>(bottomButtonMap.keySet())).build();
+	}
+
+	private void initBusinessComponent() {
+		// Get scanner from application.
+		scanner = ((MegaDictApp) getApplication()).scanner;
+
+		// Get saved instance.
+		final BusinessComponent bc = (BusinessComponent)getLastNonConfigurationInstance();
+		WordSearcher searcher;
+		WordRecommender recommender;
+		if(bc == null) {
+			// Init searcher and recommender.
+			recommender = new WordRecommender(scanner.getDictionaryModels(), dictionaryComponent);
+			searcher = new WordSearcher(scanner.getDictionaryModels(), dictionaryComponent);
+			// Init business component.
+			businessComponent =	new BusinessComponent(searcher, recommender);
+		} else {
+			businessComponent = bc;
+			recommender = businessComponent.getRecommender();
+			searcher = businessComponent.getSearcher();
+		}
+
+		// Register observers for scanner.
+		scanner.addObserver(searcher);
+		scanner.addObserver(recommender);
+	}
+
+	private void initInitializers() {
+		// Init search button.
+		final SearchButtonInitializer searchButtonInitializer =
+				new SearchButtonInitializer(businessComponent, dictionaryComponent);
+		searchButtonInitializer.init();
+
+		// Init pronounce button.
+		pronounceButtonInitializer = new PronounceButtonInitializer(dictionaryComponent);
+
+		// Init search bar.
+		final SearchBarInitializer searchBarInitializer =
+				new SearchBarInitializer(businessComponent, dictionaryComponent);
+		searchBarInitializer.init();
+		searchBarInitializer.addObserver(businessComponent.getRecommender());
+
+		// Init Result view.
+		final ResultViewInitializer resultViewInitializer =
+				new ResultViewInitializer(businessComponent, dictionaryComponent);
+		resultViewInitializer.init();
+		resultViewInitializer.addObserver(businessComponent.getRecommender());
+
+		// Init history retriever.
+		historyDisplayer = new HistoryDisplayer(businessComponent, dictionaryComponent);
+		historyDisplayer.init();
+		historyDisplayer.addObserver(businessComponent.getRecommender());
+
+		// Init result panel.
+		final LinearLayout resultPanel =
+				(LinearLayout) findViewById(R.id.resultPanel);
+		resultPanel.addView(resultView);
+
+		// Init bottom buttons.
+		final BottomButtonsInitializer bottomInitializer = new BottomButtonsInitializer(historyDisplayer, businessComponent, dictionaryComponent);
+		bottomInitializer.init();
+	}
+
 }
