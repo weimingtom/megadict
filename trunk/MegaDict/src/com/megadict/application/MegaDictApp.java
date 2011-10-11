ad2c3969727f957e774ef0546cd957ab9c8eb1f1
hieuvui214@yahoo.com <hieuvui214@yahoo.com@dd0a8cbd-19d6-256d-402b-60c49949765a>
2011-10-11 18:03:00 +0000

    Fix bug when terminate MegaDict then start it, menu doesn't show right language.

--- trunk/MegaDict/src/com/megadict/application/MegaDictApp.java
+++ trunk/MegaDict/src/com/megadict/application/MegaDictApp.java
@@ -1,21 +1,38 @@
 package com.megadict.application;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
 
 import com.megadict.business.ExternalStorage;
 import com.megadict.business.scanning.DictionaryScanner;
+import com.megadict.preferences.LanguagePreference;
+import com.megadict.utility.Utility;
 
 public final class MegaDictApp extends Application {
+	public static final String TAG = "MegaDictApp";
 	public DictionaryScanner scanner;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		Log.d(TAG, "onCreate");
+
 		// Call this to create megadict folder.
 		ExternalStorage.getExternalDirectory();
 
 		// Prepare application variables.
 		scanner = new DictionaryScanner(this);
 		scanner.scanStorage();
+
+		// Load language when first start app.
+		Utility.updateLocale(getBaseContext(), LanguagePreference.newInstance(this).getLanguage());
+	}
+
+	@Override
+	public void onConfigurationChanged(final Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		Log.d(TAG, "onConfigurationChanged");
+		Utility.updateLocale(getBaseContext(), LanguagePreference.newInstance(this).getLanguage());
 	}
 }
